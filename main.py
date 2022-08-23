f0db4d66193c8744166d1dbd837f48ebf6080c37
Damesdev <damianadanulloa@gmail.com>
2022-08-22 18:59:15 -0700

    Updated code to improve states to learn code using list comprehension. Also updated game logic.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import turtle
 import pandas
 import pandas as pd
-
 from state_names import States_score
+
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
@@ -18,11 +18,14 @@ state_data = pandas.read_csv(states_csv, index_col=False)
 states_list = state_data["state"].to_list()
 # print(states_list)
 
+score = len(states_score.correct_guessed_states)
 # game logic
-answer_state = screen.textinput(title="Guess the state", prompt="Whats another state's name?").title()
+# answer_state = screen.textinput(title="Guess the state", prompt="Whats another state's name?").title()
 
-while is_game_on:
-
+while score < 50:
+    correct = states_score.amount_correct()
+    amount_states = len(states_list)
+    answer_state = screen.textinput(title=f"{correct}/{amount_states} States Correct", prompt="Whats another state's name?").title()
 
     if answer_state in states_list:
         states_score.correct_answer(answer=answer_state)
@@ -34,26 +37,10 @@ while is_game_on:
         # print(y_cor)
         states_score.write_state(answer=answer_state, x= x_cor, y= y_cor)
 
-    correct = states_score.amount_correct()
-    amount_states = len(states_list)
-    answer_state = screen.textinput(title=f"{correct}/{amount_states} States Correct", prompt="Whats another state's name?").title()
-
-    if len(states_score.correct_guessed_states) > 50 or answer_state == "Exit" or "exit":
-        is_game_on = False
-
-
-#missed states to learn.csv
-missing_states = []
-for state in states_list:
-    if state in states_score.correct_guessed_states:
-        pass
-    else:
-        missing_states.append(state)
-
-missing = {'Missing States': missing_states}
-
-missing_df = pd.DataFrame(missing)
-
-missing_df.to_csv('states_to_learn.csv')
+    if answer_state == "Exit" or answer_state == "exit":
+        missing_states = [state for state in states_list if state not in states_score.correct_guessed_states]
+        missing_df = pd.DataFrame(missing_states)
+        missing_df.to_csv('states_to_learn.csv')
+        break
 
 turtle.mainloop()
